c234f3003d658365f383d288bd89f162ba30ac71
Boris Skurikhin <boriskurikhin@gmail.com>
2020-06-25 16:23:26 -0400

    update setup stuff

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
-from distutils.core import setup
+from setuptools import setup, Extension
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'pyuniqid',
     packages = ['pyuniqid'],
-    version = '0.1',
+    version = '1.0',
     license='MIT',
     description = 'A Unique Hexatridecimal ID generator',
-    long_description ='A Unique Hexatridecimal ID generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'Boris Skurikhin',
     author_email = 'boriskurikhin@gmail.com',
     url = 'https://github.com/boriskurikhin/pyuniqid',
-    download_url = 'https://github.com/boriskurikhin/pyuniqid/archive/v_01.0.tar.gz',
+    download_url = 'https://github.com/boriskurikhin/pyuniqid/archive/v_01.1.zip',
     keywords = ['unique', 'id', 'unique identifier', 'hexatridecimal', 'unique id', 'uniqid'],
     install_requires=[
         'netifaces',
         'numpy'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
