32e91aa3babf1c592656e26f6a96aae31b4fa21d
Boris Skurikhin <boriskurikhin@gmail.com>
2020-06-25 15:47:19 -0400

    update download url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Boris Skurikhin',                   # Type in your name
     author_email = 'boriskurikhin@gmail.com',      # Type in your E-Mail
     url = 'https://github.com/boriskurikhin/pyuniqid',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/boriskurikhin/pyuniqid/archive/v_01.tar.gz',    # I explain this later on
+    download_url = 'https://github.com/boriskurikhin/pyuniqid/archive/v_01.0.tar.gz',    # I explain this later on
     keywords = ['unique', 'id', 'unique identifier', 'hexatridecimal', 'unique id', 'uniqid'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
         'netifaces',
